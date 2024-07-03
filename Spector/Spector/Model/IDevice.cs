84c44550f1e0aaeea897d83d5dd8599d042fb668
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-07-03 16:31:48 +0900

    wip

--- Spector/Spector/Model/IDevice.cs
+++ Spector/Spector/Model/IDevice.cs
@@ -1,8 +1,11 @@
-﻿namespace Spector.Model;
+﻿using NAudio.CoreAudioApi;
+
+namespace Spector.Model;
 
 public interface IDevice : IDisposable
 {
     DeviceId Id { get; }
+    DataFlow DataFlow { get; }
     string Name { get; }
     Decibel Level { get; }
 }
