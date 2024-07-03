df63583b98d4c208cd350d2361c3d6c32ceaaffa
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-07-04 06:43:24 +0900

    wip

--- Spector/Spector/View/DeviceList.xaml.cs
+++ Spector/Spector/View/DeviceList.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,24 @@ namespace Spector.View
         public DeviceList()
         {
             InitializeComponent();
+            AudioDataGrid.Loaded += AudioDataGrid_Loaded;
+        }
+
+        private void AudioDataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            var dataFlow = AudioDataGrid.Columns[0]; // 0番目の列（DataFlow列）を指定
+            var name = AudioDataGrid.Columns[1]; // 1番目の列（Name列）を指定
+            AudioDataGrid.Items.SortDescriptions.Clear();
+            AudioDataGrid.Items.SortDescriptions.Add(new SortDescription(dataFlow.SortMemberPath, ListSortDirection.Descending));
+            AudioDataGrid.Items.SortDescriptions.Add(new SortDescription(name.SortMemberPath, ListSortDirection.Ascending));
+
+            foreach (var col in AudioDataGrid.Columns)
+            {
+                col.SortDirection = null;
+            }
+            dataFlow.SortDirection = ListSortDirection.Descending;
+            name.SortDirection = ListSortDirection.Ascending;
+            AudioDataGrid.Items.Refresh();
         }
     }
 }
