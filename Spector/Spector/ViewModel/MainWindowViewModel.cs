31ae65932a88c3005baa76029609bdc97a77a3ca
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-07-03 16:26:44 +0900

    wip

--- Spector/Spector/ViewModel/MainWindowViewModel.cs
+++ Spector/Spector/ViewModel/MainWindowViewModel.cs
@@ -1,22 +1,10 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using LiveChartsCore.Defaults;
-using LiveChartsCore.SkiaSharpView.Painting;
-using LiveChartsCore.SkiaSharpView;
-using LiveChartsCore;
-using SkiaSharp;
-using System.Collections.ObjectModel;
 using Kamishibai;
-using Reactive.Bindings;
-using Spector.Model;
 
 namespace Spector.ViewModel;
 
 public partial class MainWindowViewModel : ObservableObject, INavigatedAsyncAware, IDisposable
 {
-    public MainWindowViewModel()
-    {
-    }
-
     public AudioInterfaceViewModel AudioInterface { get; } = new();
 
     public async Task OnNavigatedAsync(PostForwardEventArgs args)
