e60ab3d1d858ffb70d9a1a31b1deeb3bfc713574
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-07-13 10:38:18 +0900

    wip

--- Spector/Spector/View/Analysis/AnalysisTab.xaml.cs
+++ Spector/Spector/View/Analysis/AnalysisTab.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace Spector.View.Analysis
 {
@@ -10,6 +12,21 @@ namespace Spector.View.Analysis
         public AnalysisTab()
         {
             InitializeComponent();
+        }
+
+        private void RecordGrid_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var dataGrid = (DataGrid)sender;
+            var dataFlow = dataGrid.Columns[0]; // 0番目の列（DataFlow列）を指定
+            dataGrid.Items.SortDescriptions.Clear();
+            dataGrid.Items.SortDescriptions.Add(new SortDescription(dataFlow.SortMemberPath, ListSortDirection.Descending));
+
+            foreach (var col in dataGrid.Columns)
+            {
+                col.SortDirection = null;
+            }
+            dataFlow.SortDirection = ListSortDirection.Descending;
+            dataGrid.Items.Refresh();
         }
     }
 }
