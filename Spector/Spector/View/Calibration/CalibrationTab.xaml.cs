5f10c7e99aabbe15076bc7e08aa019d6f54aeddd
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-08-01 03:53:00 +0900

    wip

---
+++ Spector/Spector/View/Calibration/CalibrationTab.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Spector.View.Calibration
+{
+    /// <summary>
+    /// CalibrationTab.xaml の相互作用ロジック
+    /// </summary>
+    public partial class CalibrationTab
+    {
+        public CalibrationTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
